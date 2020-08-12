5af73f0a3feb9b1400724fe922deba158772f485
Сергей Лещенко <issues13@live.com>
2020-08-12 12:55:34 +0200

    Second Commit
    Add sripts and change scene

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour 
+{
+	private void Update ()
+	{
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+
+		transform.Rotate(new Vector3(v, 0, h));
+	}
+}
